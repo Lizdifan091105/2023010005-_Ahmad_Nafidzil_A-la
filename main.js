58223a633f209fc03ed685c67da8b493ccd22de9
lizdifan091105 <ahmadala0905@gmail.com>
2024-03-20 08:48:43 +0700

    commit pertama

---
+++ main.js
@@ -0,0 +1,9 @@
+Alert ("hello ")
+Alert ("mohon isi data anda")
+// digunakan untuk menampilkqn pop up berupa teks
+// command digunakan untuk mmeberikan komentar dan tidak dibaca oleh sistem
+let nama =prompt("Masukkan Nama Anda ")
+let usia =prompt("Berapa Usia Anda ")
+let tinggibadan =prompt("berapa usia anda")
+let jenisKelamin =prompt("Apa jenis Kelamin Anda")
+Alert (`nama anda adalah ${nama} usia anda adalah ${usia} tinggi badan anda adalah ${tinggibadan} cm  gender anda adalah ${gender}`)
