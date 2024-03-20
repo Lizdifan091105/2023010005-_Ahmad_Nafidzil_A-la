11cd9cf2ab3ed9a4c58b1155b7c8b18fbe1c497f
lizdifan091105 <ahmadala0905@gmail.com>
2024-03-20 09:37:42 +0700

    commit pertama

---
+++ try.js
@@ -0,0 +1,9 @@
+alert ("hall0")
+alert ("mohon isi data diri anda ")
+let nama = prompt("Masukkan Nama Anda ")
+let usia = prompt("Masukkan Usia Anda ")
+let tinggiBadan = prompt ("Masukkan tinggi badan ")
+let jenisKelamin = prompt ("masukan jenis kelamin ")
+
+
+Alert (`Nama Anda adalah ${nama} usia anda adalah ${usia} Tahun tinggi badan anda adalah ${tinggiBadan} cm jenis kelamin anda adalah ${jenisKelamin}`)
